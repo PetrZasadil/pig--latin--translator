d8c83658c15c52995233f173605102fb97060628
PetrZasadil <petr.zasadil@email.cz>
2017-09-13 09:38:26 +0200

    Revert "Logger added"
    This reverts commit 11ee112dd6c14c978db42930a0a5efb184a4f6f2.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-date_default_timezone_set("Europe/Prague");
-
 require_once  __dir__ . '../../vendor/autoload.php';
 
 //--- autoload classes
@@ -9,7 +7,6 @@ spl_autoload_register(function ($className){
     include '../'. $className . '.php';
 });
 
-require_once  __dir__ . '../../app/config/config.php';
 
 $app = new \App\HomePage();
 $app->renderHomePage();
